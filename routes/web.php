f09a6b40e9bf7d17e5c797abe49352056960777b
marinasvirel <marusenka_83@mail.ru>
2025-11-01 04:26:51 +0300

    переименование welcome в home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
 
 Route::get('login', [UserController::class, 'login'])->name('login');
