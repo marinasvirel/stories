0f488d9446e800f7b6256c6bbb8b2889c858ca20
marinasvirel <marusenka_83@mail.ru>
2025-11-19 23:13:25 +0300

    компонент posts

---
+++ resources/views/components/stories.blade.php
@@ -0,0 +1,25 @@
+<section class="stories">
+  <ul class="stories-list">
+    @foreach ($posts as $post)
+    <li class="stories-item">
+      <h2>{{ $post->title }}</h2>
+      <ul class="stories-tags">
+        @foreach($post->tags as $tag)
+        <li class="stories-tags-item">{{ $tag->name }}</li>
+        @endforeach
+      </ul>
+      <div class="stories-content-box">
+        <div class="stories-img-box">
+          <img class="stories-img" src="{{ asset('storage/' . $post->img) }}" alt="{{ $post->title }}">
+        </div>
+        <div class="stories-text-box">
+          <div class="stories-text-container">
+            <p class="stories-text">{{ $post->text }}</p>
+          </div>
+          <a href="{{ url($linkPrefix . $post->id) }}" class="main-link">Читать</a>
+        </div>
+      </div>
+    </li>
+    @endforeach
+  </ul>
+</section>
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,18 +2,9 @@
 @section('title', 'Интересные истории | Модерация')
 @section('content')
 Список неопубликованных историй
-
-@foreach ($posts as $post)
-<h2>{{ $post->title }}</h2>
-<p>{{ $post->text }}</p>
-<img src="{{ asset('storage/' . $post->img) }}" alt="{{ $post->name }}">
-<img src="{{ $post->img }}" alt="img">
-<ul>
-  @foreach($post->tags as $tag)
-  <li>{{ $tag->name }}</li>
-  @endforeach
-</ul>
-<a href="moderation/post/{{ $post->id }}" class="main-link">Читать</a>
-@endforeach
-
+@if(isset($posts))
+<x-stories :posts="$posts" link-prefix="moderation/post/"/>
+@else
+<p>Переменная $posts не определена в родительском шаблоне.</p>
+@endif
 @endsection
