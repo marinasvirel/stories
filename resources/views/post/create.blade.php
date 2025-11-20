ee005697115985aa4b5eae6b582b9372a237b97f
marinasvirel <marusenka_83@mail.ru>
2025-11-21 02:12:47 +0300

    форма стилизация

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -10,21 +10,26 @@
   </ul>
   @endif
 </div>
-<form action="" method="post" enctype="multipart/form-data">
+<form class="create-post" action="" method="post" enctype="multipart/form-data">
   @csrf
-  <input type="text" name="author_name" placeholder="Имя" value="{{ old('name') }}" autocorrect autofocus>
-  <input type="email" name="author_email" placeholder="E-mail" value="{{ old('email') }}">
-  <input type="text" name="title" placeholder="Заголовок" value="{{ old('title') }}">
-  <textarea name="text" id="" autocorrect placeholder="Текст"></textarea>
+  <label for="author_name">Ваше имя</label>
+  <input type="text" name="author_name" placeholder="Саша" value="{{ old('name') }}" autocorrect autofocus>
+  <label for="author_email">Ваш email</label>
+  <input type="email" name="author_email" placeholder="sasha@mail.com" value="{{ old('email') }}">
+  <label for="title">Заголовок</label>
+  <input type="text" name="title" placeholder="Три кита" value="{{ old('title') }}">
+  <label for="text">Текст</label>
+  <textarea name="text" id="" autocorrect placeholder="Давным-давно..." rows="5"></textarea>
+  <label for="img">Иллюстрация</label>
   <input name="img" type="file" accept="image/*">
   <label for="existing_tags">Выбрать существующие теги:</label>
-  <select name="existing_tags[]" id="existing_tags" multiple>
+  <select name="existing_tags[]" id="existing_tags" size="1" multiple>
     @foreach($tags as $tag)
     <option value="{{ $tag->id }}">{{ $tag->name }}</option>
     @endforeach
   </select>
   <label for="new_tags">Добавить новые теги (через запятую):</label>
-  <input type="text" name="new_tags" id="new_tags" placeholder="например: php, laravel, js">
+  <input type="text" name="new_tags" id="new_tags" placeholder="приключения, путешествия">
   <button type="submit">Добавить</button>
 </form>
 @endsection
