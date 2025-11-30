11aaa93869f36ee0211c1bd344b081d129469463
marinasvirel <marusenka_83@mail.ru>
2025-11-30 14:06:36 +0300

    прижать footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,32 +9,34 @@
 </head>
 
 <body>
-  <header>
-    <div class="container container-space">
-      <a href="{{ route('home') }}" class="logo-link">
-        <img src="{{ asset('logo.png') }}" alt="logo">
-      </a>
-      <a href="{{ route('createView') }}" class="header-link">Добавить историю</a>
-    </div>
-  </header>
-  <main>
-    <div class="container">
-      @yield('content')
-    </div>
-  </main>
-  <footer>
-    <div class="container container-space">
-      <span class="creator">© Зинченко Марина, 2025</span>
-      @auth
-      <a href="{{ route('logout') }}" class="footer-link">Выход для модератора</a>
-      <a href="{{ route('readModer') }}" class="footer-link">Админка</a>
-      @endauth
-      @guest
-      <a href="{{ route('login') }}" class="footer-link">Вход для модератора</a>
-      @endguest
-      <img src="{{ asset('footer-img.png') }}" class="footer-img" alt="footer-img">
-    </div>
-  </footer>
+  <div class="wrapper">
+    <header>
+      <div class="container container-space">
+        <a href="{{ route('home') }}" class="logo-link">
+          <img src="{{ asset('logo.png') }}" alt="logo">
+        </a>
+        <a href="{{ route('createView') }}" class="header-link">Добавить историю</a>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        @yield('content')
+      </div>
+    </main>
+    <footer>
+      <div class="container container-space">
+        <span class="creator">© Зинченко Марина, 2025</span>
+        @auth
+        <a href="{{ route('logout') }}" class="footer-link">Выход для модератора</a>
+        <a href="{{ route('readModer') }}" class="footer-link">Админка</a>
+        @endauth
+        @guest
+        <a href="{{ route('login') }}" class="footer-link">Вход для модератора</a>
+        @endguest
+        <img src="{{ asset('footer-img.png') }}" class="footer-img" alt="footer-img">
+      </div>
+    </footer>
+  </div>
 </body>
 
 </html>
